725c8fdbac8c24caae7c22f8d569f00efbea3569
somannapoolanda <sachin.soman@gmail.com>
2026-03-31 19:14:13 -0500

    adding terra CI modules

---
+++ terra-modules-gitlab_ci/modules/network/locals.tf
@@ -0,0 +1,7 @@
+locals {
+  DeployedBy = "Terraform"
+  Owner      = "us-Dev-Team"
+  Costcenter = "us-8080"
+  TeamDL     = "necromonger@gmail.com"
+}
+
---
+++ terra-modules-gitlab_ci/production/infrastructure.tf
@@ -0,0 +1,60 @@
+module "prod_vpc_1" {
+  source             = "../modules/network"
+  vpc_cidr           = var.vpc_cidr
+  vpc_name           = var.vpc_name
+  environment        = var.environment
+  public_cidr_block  = var.public_cidr_block
+  private_cidr_block = var.private_cidr_block
+  azs                = var.azs
+  natgw_id           = module.prod_natgw_1.natgw_id
+}
+
+module "prod_sg_1" {
+  source        = "../modules/sg"
+  vpc_name      = module.prod_vpc_1.vpc_name
+  vpc_id        = module.prod_vpc_1.vpc_id
+  environment   = module.prod_vpc_1.environment
+  service_ports = ["80", "443", "445", "8080", "22", "3389", "1433", "3306"]
+}
+
+module "prod_natgw_1" {
+  source           = "../modules/nat"
+  public_subnet_id = module.prod_vpc_1.public_subnets_id_1
+  vpc_name         = module.prod_vpc_1.vpc_name
+}
+# module "prod_iam_1" {
+#   source              = "../modules/iam"
+#   environment         = module.prod_vpc_1.environment
+#   rolename            = "SaiTMRole"
+#   instanceprofilename = "SaiTMinstprofile"
+# }
+
+# module "prod_elb_1" {
+#   source          = "../modules/elb"
+#   environment     = module.prod_vpc_1.environment
+#   nlbname         = "prod-nlb"
+#   subnets         = module.prod_vpc_1.public_subnets_id
+#   tgname          = "prod-nlb-tg"
+#   vpc_id          = module.prod_vpc_1.vpc_id
+#   private_servers = module.prod_compute_1.private_servers
+# }
+
+# module "prod_elb_1_public" {
+#   source          = "../modules/elb"
+#   environment     = module.prod_vpc_1.environment
+#   nlbname         = "prod-nlb-public"
+#   subnets         = module.prod_vpc_1.public_subnets_id
+#   tgname          = "prod-nlb-tg-public"
+#   vpc_id          = module.prod_vpc_1.vpc_id
+#   private_servers = module.prod_compute_1.public_servers
+# }
+
+# module "prod_vpc_2" {
+#   source             = "../modules/network"
+#   vpc_cidr           = "172.30.0.0/16"
+#   vpc_name           = "prod_vpc_2"
+#   environment        = "Production"
+#   public_cidr_block  = ["172.30.1.0/24", "172.30.2.0/24", "172.30.3.0/24"]
+#   private_cidr_block = ["172.30.10.0/24", "172.30.20.0/24", "172.30.30.0/24"]
+#   azs                = ["us-east-1a", "us-east-1b", "us-east-1c"]
+# }
